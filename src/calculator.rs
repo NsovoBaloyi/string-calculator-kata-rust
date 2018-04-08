b2950dea40ff4ad5cb09a1ff5d24f9d5a58234cb
Nsovo Baloyi <NsovoBaloyi@users.noreply.github.com>
2018-04-08 03:19:13 +0200

    Refactors to solve all custom delimiter test cases

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::vec::Vec;
+use std::ops::Add;
 
 pub fn add(numbers:&str) -> i32 {
 
@@ -13,16 +14,20 @@ pub fn add(numbers:&str) -> i32 {
 
 fn parse_numbers(numbers:&str) -> Vec<i32>{
     let mut del = r"[,|\n]";
-    let values;
-    
-    if numbers.starts_with("//"){
+    let mut values;
+
+    if numbers.starts_with("//["){
+        del = &numbers[2..numbers.find("\n").unwrap()];
+        values = &numbers[numbers.find("\n").unwrap()+1..];
+    }
+    else if numbers.starts_with("//"){
         del = &numbers[2..3];
         values = &numbers[4..];
     }else {
         values = &numbers;
     }
 
-    let re = Regex::new(del).unwrap();
+    let re = Regex::new(&*del.replace("][", "|").add("+")).unwrap();
 
     re.split(values).map(|i| i.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>()
 }
