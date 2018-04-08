1b2526f8ab46ddd79c639431b9183c32bcbf3883
Nsovo Baloyi <NsovoBaloyi@users.noreply.github.com>
2018-04-08 03:08:25 +0200

    Refactors code

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,12 +1,22 @@
 use regex::Regex;
+use std::vec::Vec;
 
 pub fn add(numbers:&str) -> i32 {
-    let re = Regex::new(r"[,]+").unwrap();
 
     if numbers.is_empty(){
         0
     }
     else{
-         re.split(numbers).map(|i| i.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>().into_iter().sum()
+         sum_list(parse_numbers(numbers))
     }
+}
+
+fn parse_numbers(numbers:&str) -> Vec<i32>{
+    let re = Regex::new(r"[,]+").unwrap();
+
+    re.split(numbers).map(|i| i.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>()
+}
+
+fn sum_list(list:Vec<i32>) -> i32 {
+    list.into_iter().sum()
 }
